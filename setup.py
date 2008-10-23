61a9c75a12f15b82e5dd08f6938db2dacbffeb15
Robert Jordens <jordens@debian.org>
2008-10-23 20:29:02 +0200

    add rules content to readme

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "labjacku12",
-        version = "0.1",
+        version = "0.2",
         author = "Robert Jordens",
         author_email = "jordens@phys.ethz.ch",
         description = "LabJack U12 driver",
@@ -19,7 +19,7 @@ setup(
         url = "http://www.phys.ethz.ch/~robertjo/labjacku12",
         packages = find_packages(),
         # test_suite = "tests", #.labjacku12_tests.LabjackU12Tests",
-        scripts = ['scripts/u12_scope.py'],
+        scripts = ['scripts/*.py'],
         include_package_data = True,
         zip_safe = True,
         )
