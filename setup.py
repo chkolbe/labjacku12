0af3acb8862204a50b1a6bdb0f9db9118c7dd810
Robert Jordens <jordens@debian.org>
2009-07-23 22:00:13 +0200

    setup.py: new version 0.3, add scripts explicitly

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "labjacku12",
-        version = "0.2",
+        version = "0.3",
         author = "Robert Jordens",
         author_email = "jordens@phys.ethz.ch",
         description = "LabJack U12 driver",
@@ -19,7 +19,7 @@ setup(
         url = "http://www.phys.ethz.ch/~robertjo/labjacku12",
         packages = find_packages(),
         # test_suite = "tests", #.labjacku12_tests.LabjackU12Tests",
-        scripts = ['scripts/*.py'],
+        scripts = ['scripts/u12_scope.py', 'scripts/u12_rrd.py'],
         include_package_data = True,
         zip_safe = True,
         )
