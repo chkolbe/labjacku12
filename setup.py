26fb8416cec911e9da8df9b5de179a35da3ce7bb
Christopher Kolbe <kontakt@christopherkolbe.de>
2020-05-09 20:46:09 +0200

    Updated Project from Fork

--- setup.py
+++ setup.py
@@ -4,22 +4,21 @@ from setuptools import setup, find_packages
 
 setup(
         name = "labjacku12",
-        version = "0.4",
-        author = "Robert Jordens",
-        author_email = "robert@joerdens.org",
-        description = "LabJack U12 driver",
+        version = "0.4.1",
+        author = "Christopher Kolbe",
+        author_email = "kontakt@christopherkolbe.de",
+        description = "LabJack U12 driver with Python 3 and libusb-1.0 Support.",
         license = "BSD",
-        #install_requires = [],
-        #extras_require = {
-        #    "gadgets": ["numpy", "Chaco2",],
-        #    },
+        install_requires = [
+            'libusb1>=1.8'
+        ],
         dependency_links = [
             "http://code.enthought.com/enstaller/eggs/source",
             ],
-        url = "http://www.phys.ethz.ch/~robertjo/labjacku12",
+        url = "https://github.com/chkolbe/labjacku12",
         packages = find_packages(),
-        # test_suite = "tests", #.labjacku12_tests.LabjackU12Tests",
-        scripts = ['scripts/u12_scope.py', 'scripts/u12_rrd.py'],
+        test_suite = "tests", #.labjacku12_tests.LabjackU12Tests",
+        #scripts = ['scripts/u12_scope.py', 'scripts/u12_rrd.py'],
         include_package_data = True,
         zip_safe = True,
         )
